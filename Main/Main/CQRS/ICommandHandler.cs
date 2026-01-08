85bc6a1ad6bc63ba6fea43b57b19496aa6e21047
Djordje Maljkovic <94576107+djordjemaljkovic@users.noreply.github.com>
2026-01-08 12:00:39 +0000

    Add files via upload

---
+++ Main/Main/CQRS/ICommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main.CQRS
+{
+    public interface ICommandHandler<in TCommand> : ICommandHandler<TCommand, Unit> where TCommand : ICommand<Unit>;
+
+    public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand , TResponse> where TCommand : ICommand<TResponse>
+    {
+    }
+}
